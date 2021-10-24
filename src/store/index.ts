1da8bbbcbf024b6c31912d4c7edcbb595083eb12
Cliff Chen <786102367@qq.com>
2021-10-24 18:15:44 +0800

    init project

---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+import app from './modules/app'
+import settings from './modules/setting'
+
+export default createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    app,
+    settings
+  },
+  getters: {
+  }
+})
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data () {
+    return {
+      title: 'This is About page'
+    }
+  }
+})
+
+</script>
